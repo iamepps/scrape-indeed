9175fe3ca68d24ae3aec55b4732fdb02342d8f69
iamepps <andrew.epps@tesglobal.com>
2020-11-11 11:34:46 +0000

    append to existing file

--- src/gcs.py
+++ src/gcs.py
@@ -1,4 +1,7 @@
 from google.cloud import storage
+from uuid import uuid1
+import os
+import json
 
 class Gcs:
     def __init__(self, bucket, **kwargs):
@@ -9,7 +12,11 @@ class Gcs:
         return [f.name for f in self.bucket.list_blobs(prefix=prefix)]
 
     def read(self, path='/'):
-        return self.bucket.get_blob(path).download_as_string()
+        blob = self.bucket.get_blob(path)
+        if blob:
+            return blob.download_as_string()
+        else:
+            return None #Make this a warning? Or exception?
 
     def write(self, path, file_body):
         blob = self.bucket.blob(path)
@@ -27,3 +34,21 @@ class Gcs:
     def compose(self, prefix, outfile_path):
         blobs = self.bucket.list_blobs(prefix=prefix)
         self.bucket.blob(outfile_path).compose(blobs)
+
+    def append(self, path, new_list):
+        file_type = path.split('.')[-1]
+
+        if file_type == 'jsonl':
+            target = self.read(path).split('\n')
+
+        if file_type == 'json':
+            target = json.loads(self.read(path))
+
+        if isinstance(target, list):
+            target.extend(new_list)
+            tmp_path = f'tmp/{uuid1()}'
+            output = json.dumps(target) if file_type=='json' else '\n'.join([json.dumps(x) for x in target])
+            self.write(tmp_path, output)
+            if len(self.list_keys(tmp_path))>0:
+                self.rewrite(path, tmp_path)
+            self.delete(tmp_path)
